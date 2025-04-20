fd98b51e2f67b606e14110be14fa6e2a434535e5
dhruv <dhruvjain137@gmail.com>
2025-04-21 03:44:31 +0530

    18April-useCallback

--- 18April_useCallback/task1/src/components/UserList.js
+++ 18April_useCallback/task1/src/components/UserList.js
@@ -1,11 +1,11 @@
 import React from "react";
-const UserList = React.memo(({users,onDelete})=>{
+const UserList = React.memo(({users,onDeleteuser})=>{
     return(
         <ul className="list-disc pl-5 space-y-2">
             {users.map((user ,index)=>(
                 <li key={index} className="flex justify-between items-center">
                     <span>{user}</span>
-                    <button onClick={()=>onDelete(index)} className="text-red-500 hover:underline"
+                    <button onClick={()=>onDeleteuser(index)} className="text-red-500 hover:underline"
                     >
                         Delete
                     </button>
